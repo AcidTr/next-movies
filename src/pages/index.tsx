6843e63e8b6f77b383f08ca566b726a1f86741ed
Victor Mourão <victor_murati@hotmail.com>
2021-06-12 09:08:42 -0300

    initial config using nextjs and global style using sass

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Início | Movies</title>
+      </Head>
+      <h1>
+        Hello, new colleagues!
+      </h1>
+    </>
+  )
+}
