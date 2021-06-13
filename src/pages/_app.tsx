337491463666bdc1e5a401e0cdf3bacd3670c8ad
Victor Mourão <victor_murati@hotmail.com>
2021-06-13 14:35:37 -0300

    adding nprogress and configuring loading

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,35 @@
 import { AppProps } from 'next/app'
+import Router from 'next/router';
+import { useEffect } from 'react'
+import NProgress from 'nprogress';
+import "nprogress/nprogress.css";
+
 import { Header } from '../components/Header'
 import { FavoriteProvider } from '../hooks/useFavorites'
 
 import '../styles/global.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+    useEffect(() => {
+        NProgress.configure({
+            minimum: 0.3,
+            easing: 'ease',
+            speed: 800,
+        });
+
+
+        Router.events.on('routeChangeStart', () => NProgress.start());
+        Router.events.on('routeChangeComplete', () => NProgress.done());
+        Router.events.on('routeChangeError', () => NProgress.done());
+
+        return () => {
+            Router.events.off('routeChangeStart', () => NProgress.start());
+            Router.events.off('routeChangeComplete', () => NProgress.done());
+            Router.events.off('routeChangeError', () => NProgress.done());
+        }
+    }, []);
+
     return (
         <>
             <Header />
