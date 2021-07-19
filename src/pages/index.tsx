aec347af4f3fa4303c7f520403605bb7978b0f12
Victor Mourão <victor_murati@hotmail.com>
2021-07-19 10:05:22 -0300

    fix popular movies api request crash

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,15 @@
-import Head from 'next/head';
-import styles from './home.module.scss';
-import { FiSearch } from 'react-icons/fi'
-import { useEffect, useMemo } from 'react';
-import NProgress from 'nprogress';
+import Head from "next/head";
+import styles from "./home.module.scss";
+import { FiSearch } from "react-icons/fi";
+import { useEffect, useMemo } from "react";
+import NProgress from "nprogress";
 
-import api from '../services/api';
-import { useState } from 'react';
-import { Movie } from '../components/Movie';
-import { useCallback } from 'react';
-import { useFavorite } from '../hooks/useFavorites';
-import { useSearch } from '../hooks/useSearch';
+import api from "../services/api";
+import { useState } from "react";
+import { Movie } from "../components/Movie";
+import { useCallback } from "react";
+import { useFavorite } from "../hooks/useFavorites";
+import { useSearch } from "../hooks/useSearch";
 
 interface Movie {
     id: string;
@@ -20,7 +20,6 @@ interface Movie {
 }
 
 export default function Home() {
-
     const { query, setQuery, moviesResult, searchMovies } = useSearch();
 
     const { favoriteMovies } = useFavorite();
@@ -31,20 +30,28 @@ export default function Home() {
 
     useEffect(() => {
         // fetch data from The Movie DB API and format Movie poster and release date.
-        api.get('/movie/popular').then((response) => {
-            const updatedMovies: Movie[] = response.data.results.map((currentMovie) => {
-                return {
-                    id: currentMovie.id,
-                    title: currentMovie.title,
-                    posterPath: `https://www.themoviedb.org/t/p/w600_and_h900_bestv2${currentMovie.poster_path}`,
-                    releaseDate: currentMovie.release_date.substr(0, 4),
-                    isFavorite: favoriteMovies.includes(currentMovie.id),
-                }
+        api.get("/movie/popular")
+            .then((response) => {
+                const updatedMovies: Movie[] = response.data.results.map(
+                    (currentMovie) => {
+                        return {
+                            id: currentMovie.id,
+                            title: currentMovie.title,
+                            posterPath: `https://www.themoviedb.org/t/p/w600_and_h900_bestv2${currentMovie.poster_path}`,
+                            releaseDate:
+                                currentMovie.release_date?.substr(0, 4) ||
+                                "Não informado",
+                            isFavorite: favoriteMovies.includes(
+                                currentMovie.id
+                            ),
+                        };
+                    }
+                );
+                setPopularMovies(updatedMovies);
             })
-            setPopularMovies(updatedMovies);
-        }).catch((error) => {
-            console.log(error);
-        })
+            .catch((error) => {
+                console.log(error);
+            });
     }, [favoriteMovies, query]);
 
     useEffect(() => {
@@ -56,41 +63,41 @@ export default function Home() {
         setLoading(true);
         NProgress.start();
         searchMovies(query).finally(() => {
-            setLoading(false)
+            setLoading(false);
             NProgress.done();
         });
-    }, [query])
+    }, [query]);
 
     const onChangeText = useCallback(({ target }) => {
-        const text = target.value || '';
+        const text = target.value || "";
 
         setQuery(text);
-    }, [])
+    }, []);
 
     const renderSection = useMemo(() => {
-
         if (moviesResult.length <= 0 && query && !loading) {
             return (
                 <div>
                     <FiSearch className={styles.icon} />
-                    <h3>Não encontramos<br />filmes para a sua <br /> pesquisa  :(</h3>
+                    <h3>
+                        Não encontramos
+                        <br />
+                        filmes para a sua <br /> pesquisa :(
+                    </h3>
                 </div>
-            )
+            );
         }
 
         if (moviesResult.length > 0) {
             return moviesResult.map((movie: Movie) => (
                 <Movie key={movie.id} movie={movie} />
-            ))
+            ));
         }
 
         return popularMovies.map((movie: Movie) => (
             <Movie key={movie.id} movie={movie} />
-        ))
-
-
-
-    }, [moviesResult, popularMovies, query, loading])
+        ));
+    }, [moviesResult, popularMovies, query, loading]);
 
     return (
         <>
@@ -100,13 +107,17 @@ export default function Home() {
             <main className={styles.content}>
                 <div>
                     <FiSearch className={styles.icon} />
-                    <input placeholder='Pesquise filmes...' onChange={onChangeText} value={query} />
+                    <input
+                        placeholder="Pesquise filmes..."
+                        onChange={onChangeText}
+                        value={query}
+                    />
                 </div>
-                <h2>{moviesResult.length ? 'Resultados' : 'Filmes Populares'}</h2>
-                <section>
-                    {renderSection}
-                </section>
+                <h2>
+                    {moviesResult.length ? "Resultados" : "Filmes Populares"}
+                </h2>
+                <section>{renderSection}</section>
             </main>
         </>
-    )
+    );
 }
